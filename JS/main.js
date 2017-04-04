737ce5c4be80bda67235d74f478f674cbf9d2f9b
martinchaffee <martinchaffee@gmail.com>
2017-04-04 14:06:28 +0300

    Created jQuery Search Function; minor style changes
    Finished jQuery search function and changed CSS (made #gallery DIV flex
    box “justify-content: center” so search results were styled better.

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,25 @@
-    lightbox.option({
-      'showImageNumberLabel': false,
-      'maxHeight': 800,
-      'maxWidth': 800,
-      'positionFromTop': 100
-    })
-
-// Problem: Users need to be able to filter through the photos in
-// real-time as you type, and only display photos that match the search.
-// 1: User types input into seach
-//2.1: collect user input as it is typed
-//3.1: If input DOES NOT MATCH images caption text,
-//3.2: Image's style = display value = none;
-
-//Collect search input and store search input as a variable (searchInput)
-  //Put an event listener on the "input" element
-const searchInput = document.querySelector('input');
-
-searchInput.addEventListener('input', (e) => {
-  const image = document.getElementsByTagName('a');
-  const imageCaption = a.getAttribute('data-title');
-  console.log(imageCaption);
-
+// Lightbox Options
+lightbox.option({
+  'showImageNumberLabel': false,
+  'maxHeight': 750,
+  'maxWidth': 750,
+  'positionFromTop': 75,
+  'wrapAround': true
 });
-//Collect imageCaption descriptions (a's data-title) and store as variable (imageCaptions)
-//Create a function that loops through the collection of caption descriptions;
-  //captionImage = parent of
-  //If searchInput === a word or phrase in captions, parent  display = zero
+
+// Search Function Using jQuery
+$(function(){
+  $('#searchbox').keyup(function(){
+    var search = $(this).val().toLowerCase();
+
+  $('#gallery a').each(function(){
+    var text = $(this).attr('data-title');
+
+    if(text.includes(search)) {
+      $(this).show();                                 //Shows matching images
+    } else {
+      $(this).fadeOut(300).attr('data-lightbox', ''); //Hides images from gallery and slideshow
+    };
+  });
+  });
+});
